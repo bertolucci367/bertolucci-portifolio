b4a1803820a5bc28c18111d35d92a72476c02c1c
bertolucci <oficina@bertolucci.com.br>
2022-04-26 09:00:43 -0300

    feat: :sparkles: Adição de recursos
    Foi feita a adição de recursos extramamente significativos ao projeto, como contexts, hooks, libs e outras dependências.
    
    hooks; contexts; libs; customized states;

---
+++ pages/dashboard.js
@@ -0,0 +1,13 @@
+import useAuth from '../hooks/useAuth'
+
+const Dashboard = () => {
+  const { user, signout } = useAuth()
+  return (
+    <>
+      <h1>Bem vindo {user?.name} </h1>
+      <button onClick={() => signout()}>logout</button>
+    </>
+  )
+}
+
+export default Dashboard
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import useAuth from '../hooks/useAuth'
 
 export default function Home() {
+  const { user, signinGitHub, signinGoogle } = useAuth()
   return (
     <div>
       <h1>Hello World</h1>
+      <div>
+        <button onClick={() => signinGitHub()}>Entrar com Github</button>
+      </div>
+      <div>
+        <button onClick={() => signinGoogle()}>Entrar com Google</button>
+      </div>
     </div>
   )
 }
