1917c92f9087bac51f2b2387340ce7119b688fc7
jaderdev <jadersonnascimento@outlook.com>
2022-05-13 17:55:22 -0300

    Front end dos filtros

--- pages/products/index.js
+++ pages/products/index.js
@@ -26,9 +26,10 @@ import {
 import { HamburgerIcon,  AddIcon, MinusIcon, Search2Icon } from '@chakra-ui/icons'
 import Layout from 'src/components/Layout';
 import PaginatedItems from 'pages/products/paginate';
+import NextLink from "next/link"
 
 import {
-  getAllProductsThumbs, getAllProductsCount
+  getAllProductsThumbs, getAllProductsCount, getAllFilters
 } from 'src/lib/graphcms';
 
 import React, { useEffect, useState } from 'react';
@@ -94,7 +95,11 @@ function Items({ currentItems }) {
       currentItems.map((item,key) => (
         <Box flex="30%" m="10px" key={key}>
           <Center flex="1">
-            <Box flex="1" h="350px" bg="lightgray"></Box>
+            <Box flex="1" bg="lightgray"
+              border="1px solid dashed"
+            >
+              <Image src={item.photo[0].url} w="100%"/>
+            </Box>
           </Center>
           <Center flex="1" mt="10px">
             <Text fontSize="20px" textTransform="capitalize">{ item.name
@@ -110,29 +115,36 @@ function Items({ currentItems }) {
 const productsQtd = 42
 
 
-export default function Products({ products, productsCount }) {
+export default function Products({ products, productsCount, filters }) {
   
   const [ currentItems, setCurrentItems ] = useState(null);
   const [ setPageCount] = useState(0);
   const [ itemOffset, setItemOffset ] = useState(0);
+  const [ showFiltersBar, setShowFiltersBar ] = useState(false);
+  const [ showTypology, setShowTypology ] = useState(false);
+  const [ showMaterial, setShowMaterial ] = useState(false);
+  const [ showCategory, setShowCategory ] = useState(false);
+  const [ showDesigners, setShowDesigners ] = useState(false);
   const itemsPerPage = productsQtd
   const pageCount = productsCount / productsQtd
 
-  if(!currentItems){
-    setCurrentItems(products)
-  }
+
+  // ========================================================
+
+
+  if(!currentItems){ setCurrentItems(products) }
+
+  const toggleFiltersBar = () => setShowFiltersBar(!showFiltersBar)
+  const toggleTypology = () =>  setShowTypology(!showTypology)
+  const toggleMaterial = () =>  setShowMaterial(!showMaterial)
+  const toggleCategory = () =>  setShowCategory(!showCategory)
+  const toggleDesigners = () =>  setShowDesigners(!showDesigners)
 
   useEffect(() => {
-    console.log(currentItems)
   }, []);
 
   const handlePageClick = (event) => {
-    const newOffset = (event.selected * itemsPerPage) % items.length;
-    console.log(
-      `User requested page number ${event.selected}, which is offset ${newOffset}`
-    );
     getAllProductsThumbs(event.selected,productsQtd).then((x)=>{setCurrentItems(x)})
-    setItemOffset(newOffset);
   };
 
   return (
@@ -140,9 +152,13 @@ export default function Products({ products, productsCount }) {
         <Box flex='1' mt='85px'>
           <Flex>
             <Box flex='1' p="15px">
+              <NextLink href="/" passHref>
               <Link d="inline">Bertolucci</Link>
+              </NextLink>
               <Text d="inline"> | </Text>
+              <NextLink href="/products" passHref>
               <Link d="inline">Produtos</Link>
+              </NextLink>
             </Box>
             <Spacer/>
             <Box flex='1' py={2}>
@@ -176,41 +192,82 @@ export default function Products({ products, productsCount }) {
         </Box>
         <Box>
           <Flex>
-            <Button rightIcon={<HamburgerIcon />} >FILTROS</Button>
+            <Button rightIcon={ showFiltersBar ? <AddIcon/> : <MinusIcon />} onClick={toggleFiltersBar} variant='outline'>FILTROS</Button>
           </Flex>
         </Box>
         <Box w="100%">
           <Flex>
-            <Box w='200px' pr="20px">
-              {categorias.map( (items, index) =>(
-                  <Box borderBottom="2px solid lightgray" py="10px" key={index}>
-                    <Heading as="h2" size="l" pl="10px">{ items.nome } <MinusIcon pb="3px"/></Heading>
-                    <List p="15px">
-                      { items.items.map((x1, y1) => (
-                          <ListItem key={y1}>
-                            <Checkbox mr="20px" mt="3px"></Checkbox>{x1}
-                          </ListItem>
-                        ))
-                      }
-                    </List>
-                  </Box>
-                ))}
-            </Box>
-            <Box flex="1">
-              <Flex direction="row" wrap="wrap" w="100%">
-              <Items currentItems={currentItems} />
-              <style global jsx>{`
-                  ul.paginate li {
-                    display: inline;
-                    padding: 0px 7px;
-                    font-weight: bold;
-                    font-size: 20px;
+            <Box w='200px' pr="20px" hidden={showFiltersBar}>
+              <Button rightIcon={ showTypology ? <AddIcon/> : <MinusIcon />} onClick={toggleTypology} variant='link' color="black">
+                <Heading as="h2" size="l" pl="10px" pt="10px" pb="5px">TIPOLOGIA </Heading>
+              </Button>
+              <Box borderBottom="2px solid lightgray">
+                <List p="15px" hidden={showTypology}>
+                  { filters.typologies.map((item, key) => (
+                      <ListItem key={key} py="3px">
+                        <Checkbox mr="20px" mt="3px"></Checkbox>{item.name}
+                      </ListItem>
+                    ))
                   }
+                </List>
+              </Box>
+              <Button rightIcon={ showMaterial ? <AddIcon/> : <MinusIcon />} onClick={toggleMaterial} variant='link' color="black">
+                <Heading as="h2" size="l" pl="10px" pt="10px" pb="5px">MATERIAIS </Heading>
+              </Button>
+              <Box borderBottom="2px solid lightgray">
+                <List p="15px"  hidden={showMaterial}>
+                  { filters.materials.map((item, key) => (
+                      <ListItem key={key} py="3px">
+                        <Checkbox mr="20px" mt="3px"></Checkbox>{item.name}
+                      </ListItem>
+                    ))
+                  }
+                </List>
+              </Box>
+              <Button rightIcon={ showCategory ? <AddIcon/> : <MinusIcon />} onClick={toggleCategory} variant='link' color="black">
+                <Heading as="h2" size="l" pl="10px" pt="10px" pb="5px">CATEGORIAS </Heading>
+              </Button>
 
-                  ul.paginate li.selected {
-                    font-size: 25px;
+              <Box borderBottom="2px solid lightgray" maxHeight="300px" overflow="auto">
+                <List p="15px"  hidden={showCategory}>
+                  { filters.catalogos.map((item, key) => (
+                      <ListItem key={key} py="3px">
+                        <Checkbox mr="20px" mt="3px"></Checkbox>{item.name}
+                      </ListItem>
+                    ))
                   }
-                `}</style>
+                </List>
+              </Box>
+              <Button rightIcon={ showDesigners ? <AddIcon/> : <MinusIcon />} onClick={toggleDesigners} variant='link' color="black">
+                <Heading as="h2" size="l" pl="10px" pt="10px" pb="5px">DESIGNERS </Heading>
+              </Button>
+
+              <Box borderBottom="2px solid lightgray" maxHeight="300px" overflow="auto">
+                <List p="15px"  hidden={showDesigners}>
+                  { filters.designers.map((item, key) => (
+                      <ListItem key={key} title={item.name} textOverflow="ellipsis" whiteSpace="nowrap" overflow="hidden">
+                        <Checkbox mr="20px" mt="3px"></Checkbox>{item.name}
+                      </ListItem>
+                    ))
+                  }
+                </List>
+              </Box>
+            </Box>
+            <Box flex="1" px="10px">
+              <Flex direction="row" wrap="wrap" w="100%">
+                <Items currentItems={currentItems} />
+                <style global jsx>{`
+                    ul.paginate li {
+                      display: inline;
+                      padding: 0px 7px;
+                      font-weight: bold;
+                      font-size: 20px;
+                    }
+
+                    ul.paginate li.selected {
+                      font-size: 25px;
+                    }
+                  `}</style>
                 <Center w="100%" py="20px">
                   <ReactPaginate
                     className="paginate"
@@ -234,10 +291,12 @@ export default function Products({ products, productsCount }) {
 export const getStaticProps = async () => {
   const products = await getAllProductsThumbs(0,productsQtd)
   const productsCount = await getAllProductsCount()
+  const filters = await getAllFilters()
   return {
     props: {
       products,
-      productsCount
+      productsCount,
+      filters
     },
     revalidate: 10,
   };
