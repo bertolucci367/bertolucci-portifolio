ad60a00e74fc9573c926d80f1f9372853bcd01ea
jaderdev <jadersonnascimento@outlook.com>
2022-05-17 01:58:53 -0300

    Versão da tela de produtos individuais

--- pages/produtos/[pid].js
+++ pages/produtos/[pid].js
@@ -23,12 +23,15 @@ import {
   Link,
   InputGroup,
   InputRightElement,
+  SimpleGrid,
+  IconButton
 } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
 import Layout from 'src/components/Templates/Layout';
 import { getProduct, getAllProducts } from 'src/lib/graphcms';
 import ItemProduto from 'src/components/ItemProduto';
 import React, { useEffect, useState } from 'react';
+import { ChevronDownIcon, ChevronUpIcon } from '@chakra-ui/icons'
 
 const Produto = () => {
   const router = useRouter();
@@ -38,36 +41,113 @@ const Produto = () => {
 
   if(!pid){  return ""; }
 
- if(!product) { getProduct(pid).then(x => setProduct(x))}
-
-  console.log(product)
+  if(!product) { getProduct(pid).then(x => {setProduct(x)})}
 
   return (
     <Layout>
-      <BreakCrumb />
-      <Box>
-        <Flex flex="30%" b="lightgray">
-          <Text fontSize="4xl">{ product?.name}</Text>
-          <Text textTransform="capitalize" fontSize="2xl">
-            { product?.designer?.name}
-          </Text>
-          <Text>{product?.history}</Text>
+      <BreakCrumb pid={ pid } name={ product?.name }/>
+        <Flex flex="1" mt="40px" w="100%">
+          <Box w="40%" pl="20px" pr="40px" fontFamily="nunito">
+            <Text fontSize="4xl" textTransform="uppercase" letterSpacing="-1px" mb="-13px" >
+              { product?.name}
+            </Text>
+            <Text textTransform="capitalize" fontSize="3xl" letterSpacing="-1px">
+              {product?.designer?.name}
+            </Text>
+            <Text fontSize="" py="20px" ml="10px">História da luminária</Text>
+            <Text my="10px" ml="10px" textAlign="justify">{product?.description?.text}</Text>
+          </Box>
+          <Box w="60%">
+            <Flex w="100%">  
+              <Box w="100%">
+                <Flex w="100%" px="10px">
+                  <Center bg="lightgray" w="78%" pr="20px" ml="20px">
+                    <Image src={product?.photo[1].url} maxHeight="600px" m="0 auto"/>
+                  </Center>
+                  <Box w="20%" ml="20px" pr="30px">
+                    <IconButton icon={<ChevronUpIcon />} w="100%" mb="10px" fontSize="30px" 
+                    position="relative" bg="lightgray" float="top"></IconButton>                                        
+                    <Box overflow="hidden" maxHeight="500px" mt="">
+                      {product?.photo.map((x,y)=>(
+                        <Flex pb="10px" key={y}>
+                          <Center bg="lightgray" border="4px solid lightgray">
+                            <Image src={x.url} w="100%" m="0 auto"/>
+                          </Center>
+                        </Flex>    
+                      ))}
+                      {product?.photo.map((x,y)=>(
+                        <Flex pb="10px" key={y}>
+                          <Center bg="lightgray" border="4px solid lightgray">
+                            <Image src={x.url} w="100%" m="0 auto"/>
+                          </Center>
+                        </Flex>    
+                      ))}
+                    </Box>
+                    <IconButton icon={<ChevronDownIcon />} w="100%" mb="10px" 
+                    fontSize="30px" position="relative" bottom="-10px" bg="lightgray" 
+                    float="bottom"></IconButton>
+                  </Box>
+                </Flex>    
+              </Box>
+            </Flex>  
+          </Box>
         </Flex>
-        <Flex flex="1" b="lightblue">
-          <Text>Ficha Tecnica</Text>
-          <Text>Descrição</Text>
-          <Text>{product?.description?.text}</Text>
+        <Flex flex="1" mt="40px" w="100%">
+          <Box w="100%" px="20px">
+            <Text fontSize="4xl" textTransform="uppercase" letterSpacing="-2px">Ficha Técnica</Text>
+          </Box>
         </Flex>
-    </Box>
+        <Flex flex="1" mt="40px" w="100%">
+          <Box w="100%" pb="20px">
+            <Flex flex="1" mt="15px" pl="20px" w="100%">
+              <Box w="100%" pb="20px">
+                <Flex flex="1">
+                  <Box w="100%" px="20px">
+                    <Text fontSize="l" fontWeight="bold" pb="10px">Descrição</Text>
+                    <Text>{product?.text?.text}</Text>
+                  </Box>
+                </Flex>
+              </Box>
+              <Box w="100%" pb="20px">
+                <Flex flex="1">
+                  <Box w="100%" px="20px">
+                    <Text fontSize="l" fontWeight="bold" pb="10px">Materiais</Text>
+                    <Text>{product?.materials}</Text>
+                  </Box>
+                </Flex>
+              </Box>
+            </Flex>
+            <Flex flex="1" mt="10px" pl="20px" w="100%">
+              <Box w="100%" pb="20px">
+                <Flex flex="1">
+                  <Box w="100%" px="20px">
+                    <Text fontSize="l" fontWeight="bold" pb="10px">Designer</Text>
+                    <Text textAlign="justify">{product?.designer?.text}</Text>
+                  </Box>
+                </Flex>
+              </Box>
+              <Box w="100%" pb="20px">
+                <Flex flex="1">
+                  <Box w="100%" px="20px">
+                    <Text fontSize="l" fontWeight="bold" pb="10px">Acabamentos</Text>
+                    <Text textTransform="capitalize">{product?.finishings?.map(x => (x.name)).join(", ")}</Text>
+                  </Box>
+                </Flex>
+                </Box>
+            </Flex>
+          </Box>
+          <Box w="30%" px="20px">
+            <Text fontSize="l" fontWeight="bold" pb="10px" position="relative" top="0px">Dimensão</Text>
+            <Image src={product?.photo[1].url} maxHeight="600px" m="0 auto"/>
+          </Box>
+        </Flex>
     </Layout>
   );
 };
 
 export default Produto;
 
-const BreakCrumb = () => {
-  const pid = '';
-  const name = 'trocar';
+const BreakCrumb = ({ pid, name }) => {
   return (
     <Box flex="1" mt="85px">
       <Flex>
